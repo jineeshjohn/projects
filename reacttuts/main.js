083f9c963938ac15ff811fe81cd238ff887f781b
jjohn <jjohn@wiley.com>
2016-04-22 12:54:43 +0100

    JJ|Added minified version of react

--- reacttuts/main.js
+++ reacttuts/main.js
@@ -1,30 +1,28 @@
 var MessageView = (props) => <div>{props.message}</div>;
 
 var RandomMessage = React.createClass({
-  getInitialState: function() {
-    return { message: 'Hello, Universe' };
-  },
-  onClick: function() {
-    var messages = ['Hello, World', 'Hello, Planet', 'Hello, Universe'];
-    var randomMessage = messages[Math.floor((Math.random() * 3))];
+    getInitialState: function() {
+        return { message: 'Hello, Universe' };
+    },
+    onClick: function() {
+        var messages = ['Hello, Ealing', 'Hello, London', 'Hello, UK', 'Hello, Europe', 'Hello, Earth', 'Hello, Planet'];
+        var randomMessage = messages[Math.floor((Math.random() * 3))];
 
-    this.setState({ message: randomMessage });
-  },
-  render: function() {
-    return (
-      <div>
+        this.setState({ message: randomMessage });
+    },
+    render: function() {
+        return (
+            <div>
         <MessageView message={ this.state.message }/>
         <p><input type="button" onClick={ this.onClick } value="Change Message"/></p>
       </div>
-    );
-  }
+        );
+    }
 });
 
 
 
 ReactDOM.render(
-  <RandomMessage />,
-  document.getElementById('container')
+    <RandomMessage />,
+    document.getElementById('container')
 );
-
-
